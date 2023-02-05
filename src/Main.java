cae8b5d7f87856959be534da22feb834703ac68a
TamaraKoval <kovalpsy@mail.ru>
2023-02-05 16:52:44 +0300

    WordsCheckers complete

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,20 @@
+import checkers.WordsChecker;
+
 public class Main {
+    public static void main(String[] args) {
+        String loremIpsum = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, " +
+                "sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, " +
+                "quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. " +
+                "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat " +
+                "nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui " +
+                "officia deserunt mollit anim id est laborum.";
+
+        WordsChecker checker = new WordsChecker(loremIpsum);
+        System.out.println(checker.hasWord("ut")); // true, программа работает корректно
+        System.out.println(checker.hasWord("Toma")); // false, программа работает корректно
+
+        checker.setText("Ехал Грека через реку");
+        System.out.println(checker.hasWord("грека")); // true, программа работает корректно
+        System.out.println(checker.hasWord("рак")); // false, программа работает корректно
+    }
 }
--- src/checkers/WordsChecker.java
+++ src/checkers/WordsChecker.java
@@ -1,4 +1,20 @@
 package checkers;
 
+import java.util.HashSet;
+import java.util.List;
+
 public class WordsChecker {
+    private HashSet<String> textSet;
+
+    public WordsChecker(String text) {
+        this.textSet = new HashSet<>(List.of(text.toLowerCase().split("\\P{IsAlphabetic}+")));
+    }
+
+    public void setText(String newText) {
+        this.textSet = new HashSet<>(List.of(newText.toLowerCase().split("\\P{IsAlphabetic}+")));
+    }
+
+    public boolean hasWord(String word) {
+        return textSet.contains(word);
+    }
 }
